c03befd136101d19f3137c3016202cfb9ec33db1
Tamal Saha <tamal@appscode.com>
2020-10-10 01:36:46 -0700

    Use release-automaton@v0.0.29
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -36,7 +36,7 @@ refresh() {
     sed -i 's/Go\ 1.14/Go\ 1.15/g' *
     sed -i 's/go-version:\ 1.14/go-version:\ 1.15/g' *
     sed -i 's/go-version:\ ^1.14/go-version:\ ^1.15/g' *
-    sed -i 's/release-automaton\/releases\/download\/v0.0.27\//release-automaton\/releases\/download\/v0.0.28\//g' *
+    sed -i 's/release-automaton\/releases\/download\/v0.0.28\//release-automaton\/releases\/download\/v0.0.29\//g' *
     sed -i 's/hugo-tools\/releases\/download\/v0.2.16\//hugo-tools\/releases\/download\/v0.2.18\//g' *
     sed -i 's/hugo-tools\/releases\/download\/v0.2.17\//hugo-tools\/releases\/download\/v0.2.18\//g' *
     popd
