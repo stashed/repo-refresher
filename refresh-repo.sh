eeaa3f7dab5370b12d26790efc1112eb8bdc5451
Tamal Saha <tamal@appscode.com>
2020-10-11 10:34:01 -0700

    Drop support for k8s 1.12, 1.13
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -38,6 +38,7 @@ refresh() {
     sed -i 's|engineerd/setup-kind@v0.3.0|engineerd/setup-kind@v0.4.0|g' *
     sed -i 's|version: v0.7.0|version: v0.9.0|g' *
     sed -i 's|version: v0.8.1|version: v0.9.0|g' *
+    sed -i 's|\[v1.12.10, v1.13.12, v1.14.10, v1.15.11, v1.16.9, v1.17.5, v1.18.4\]|\[v1.14.10, v1.15.11, v1.16.9, v1.17.5, v1.18.8, v1.19.1\]|g' *
     # update GO
     sed -i 's/Go\ 1.14/Go\ 1.15/g' *
     sed -i 's/go-version:\ 1.14/go-version:\ 1.15/g' *
