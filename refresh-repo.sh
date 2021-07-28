a30ac0b283957b431801d5da058e0e5c6457bbf3
Tamal Saha <tamal@appscode.com>
2021-07-27 18:56:45 -0700

    Fix script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -40,8 +40,8 @@ refresh() {
     if [ -f go.mod ]; then
         go mod edit \
             -require=kmodules.xyz/client-go@dd0503cf99cf3b6abb635d8945a8d7d8fed901d9 \
-            -require=kubeform.dev/webhook-runtime@e489faf01981d2f3afa671989388c7b6f22b6baa \
-            -require=kubeform.dev/resource-metadata@dcc1abc08aa00646b9474f7702b45c798b3ce66c \
+            -require=kmodules.xyz/webhook-runtime@e489faf01981d2f3afa671989388c7b6f22b6baa \
+            -require=kmodules.xyz/resource-metadata@dcc1abc08aa00646b9474f7702b45c798b3ce66c \
             -replace=github.com/satori/go.uuid=github.com/gofrs/uuid@v4.0.0+incompatible \
             -replace=helm.sh/helm/v3=github.com/kubepack/helm/v3@v3.6.1-0.20210518225915-c3e0ce48dd1b \
             -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20210716212718-83e5493ac170
