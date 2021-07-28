b4d27ee731f328b0f308e990a246ad380c8c8457
Tamal Saha <tamal@appscode.com>
2021-07-27 18:53:21 -0700

    Update deps
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -4,12 +4,13 @@
 SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
-OLD_VER=0.12.0
+OLD_VER=0.12.0-ac.20210727
 NEW_VER=0.12.0-ac.20210727
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
 PR_BRANCH=stash-updater # -$(date +%s)
-COMMIT_MSG="Use restic ${NEW_VER}"
+# COMMIT_MSG="Use restic ${NEW_VER}"
+COMMIT_MSG="Update dependencies"
 
 REPO_ROOT=/tmp/stash-updater
 
@@ -35,14 +36,18 @@ refresh() {
     sed -i "s|github.com/restic/restic|github.com/stashed/restic|g" Dockerfile.dbg
     sed -i "s|github.com/restic/restic|github.com/stashed/restic|g" Dockerfile.test || true
     sed -i "s|RESTIC_VER\([[:space:]]*\):= ${OLD_VER}|RESTIC_VER\1:= ${NEW_VER}|g" Makefile
-    sed -i "s|string{\"stash-enterprise\"}|string{\"stash-enterprise\",\ \"kubedb-ext-stash\"}|g" pkg/root.go
-    go mod edit \
-        -require=kmodules.xyz/client-go@dd0503cf99cf3b6abb635d8945a8d7d8fed901d9 \
-        -require=kubeform.dev/webhook-runtime@e489faf01981d2f3afa671989388c7b6f22b6baa \
-        -require=kubeform.dev/resource-metadata@dcc1abc08aa00646b9474f7702b45c798b3ce66c \
-        -replace=github.com/satori/go.uuid=github.com/gofrs/uuid@v4.0.0+incompatible \
-        -replace=helm.sh/helm/v3=github.com/kubepack/helm/v3@v3.6.1-0.20210518225915-c3e0ce48dd1b \
-        -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20210716212718-83e5493ac170
+    sed -i "s|strng{\"stash-enterprise\"}|string{\"stash-enterprise\",\ \"kubedb-ext-stash\"}|g" pkg/root.go
+    if [ -f go.mod ]; then
+        go mod edit \
+            -require=kmodules.xyz/client-go@dd0503cf99cf3b6abb635d8945a8d7d8fed901d9 \
+            -require=kubeform.dev/webhook-runtime@e489faf01981d2f3afa671989388c7b6f22b6baa \
+            -require=kubeform.dev/resource-metadata@dcc1abc08aa00646b9474f7702b45c798b3ce66c \
+            -replace=github.com/satori/go.uuid=github.com/gofrs/uuid@v4.0.0+incompatible \
+            -replace=helm.sh/helm/v3=github.com/kubepack/helm/v3@v3.6.1-0.20210518225915-c3e0ce48dd1b \
+            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20210716212718-83e5493ac170
+        go mod tidy
+        go mod vendor
+    fi
     [ -z "$2" ] || (
         echo "$2"
         $2 || true
