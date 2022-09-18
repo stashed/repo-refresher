cd4f742ebd10be39572e17b257ef31fc06f6f31b
Tamal Saha <tamal@appscode.com>
2022-09-18 03:51:23 -0700

    restic 0.14.0
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -4,13 +4,13 @@
 SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
-OLD_VER=0.13.0
-NEW_VER=0.13.1
+OLD_VER=0.13.1
+NEW_VER=0.14.0
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
-PR_BRANCH=stash-updater # -$(date +%s)
+PR_BRANCH=r0140 # -$(date +%s)
 # COMMIT_MSG="Use restic ${NEW_VER}"
-COMMIT_MSG="Update nats client to v1.15.0"
+COMMIT_MSG="Use restic 0.14.0"
 
 REPO_ROOT=/tmp/stash-updater
 
@@ -29,7 +29,7 @@ refresh() {
     rm -rf $REPO_ROOT
     mkdir -p $REPO_ROOT
     pushd $REPO_ROOT
-    git clone --no-tags --no-recurse-submodules --depth=1 https://${GITHUB_USER}:${GITHUB_TOKEN}@$1.git
+    git clone --no-tags --no-recurse-submodules --depth=1 git@github.com:$1.git
     cd $(ls -b1)
     git checkout -b $PR_BRANCH
     # sed -i "s|github.com/restic/restic|github.com/stashed/restic|g" Dockerfile.in
@@ -39,35 +39,48 @@ refresh() {
     sed -i "s|github.com/stashed/restic|github.com/restic/restic|g" Dockerfile.dbg
     sed -i "s|github.com/stashed/restic|github.com/restic/restic|g" Dockerfile.test || true
     sed -i "s|RESTIC_VER\([[:space:]]*\):= ${OLD_VER}|RESTIC_VER\1:= ${NEW_VER}|g" Makefile
-    sed -i "s|strng{\"stash-enterprise\"}|string{\"stash-enterprise\",\ \"kubedb-ext-stash\"}|g" pkg/root.go
-    if [ -f go.mod ]; then
-        sed -i "s|go 1.12|go 1.17|g" go.mod
-        sed -i "s|go 1.13|go 1.17|g" go.mod
-        sed -i "s|go 1.14|go 1.17|g" go.mod
-        sed -i "s|go 1.15|go 1.17|g" go.mod
-        sed -i "s|go 1.16|go 1.17|g" go.mod
-        go mod edit \
-            -require=kmodules.xyz/client-go@36281a681909309a3323ba55cd20c59cf615e7df \
-            -require=kmodules.xyz/monitoring-agent-api@0290ed5b75e16eb2d3a6066851ae5570d101b6f8 \
-            -require=kmodules.xyz/webhook-runtime@0ddfc9e4c2214ebcc4acd9e33d2f8e9880de1428 \
-            -require=kmodules.xyz/resource-metadata@v0.10.15 \
-            -require=kmodules.xyz/custom-resources@237eae1d7ddd7dd2b5384b5f306aa489ef6c49ef \
-            -require=kmodules.xyz/objectstore-api@f1d593d0a778b3f502dfff9cdcb759ac5e55e6a4 \
-            -require=kmodules.xyz/offshoot-api@fefb02c26514eb8cb52b88697d0e6104f2241caf \
-            -require=go.bytebuilders.dev/license-verifier@v0.9.7 \
-            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.9.7 \
-            -require=go.bytebuilders.dev/audit@v0.0.20 \
-            -require=gomodules.xyz/x@v0.0.14 \
-            -require=gomodules.xyz/logs@v0.0.6 \
-            -replace=github.com/satori/go.uuid=github.com/gomodules/uuid@v4.0.0+incompatible \
-            -replace=github.com/dgrijalva/jwt-go=github.com/gomodules/jwt@v3.2.2+incompatible \
-            -replace=github.com/golang-jwt/jwt=github.com/golang-jwt/jwt@v3.2.2+incompatible \
-            -replace=github.com/form3tech-oss/jwt-go=github.com/form3tech-oss/jwt-go@v3.2.5+incompatible \
-            -replace=helm.sh/helm/v3=github.com/kubepack/helm/v3@v3.6.1-0.20210518225915-c3e0ce48dd1b \
-            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.21.2-0.20220112070009-e3f6e88991d9
-        go mod tidy
-        go mod vendor
-    fi
+    sed -i 's/?=\ 1.18/?=\ 1.19/g' Makefile
+    # sed -i "s|strng{\"stash-enterprise\"}|string{\"stash-enterprise\",\ \"kubedb-ext-stash\"}|g" pkg/root.go
+
+    pushd .github/workflows/ && {
+        # update GO
+        sed -i 's/Go\ 1.18/Go\ 1.19/g' *
+        sed -i 's/go-version:\ ^1.18/go-version:\ ^1.19/g' *
+        popd
+    }
+
+    # if [ -f go.mod ]; then
+    #     sed -i "s|go 1.12|go 1.17|g" go.mod
+    #     sed -i "s|go 1.13|go 1.17|g" go.mod
+    #     sed -i "s|go 1.14|go 1.17|g" go.mod
+    #     sed -i "s|go 1.15|go 1.17|g" go.mod
+    #     sed -i "s|go 1.16|go 1.17|g" go.mod
+    #     go mod edit \
+    #         -require=kmodules.xyz/client-go@v0.24.5 \
+    #         -require=kmodules.xyz/monitoring-agent-api@v0.24.0 \
+    #         -require=kmodules.xyz/webhook-runtime@v0.24.0 \
+    #         -require=kmodules.xyz/custom-resources@v0.24.1 \
+    #         -require=kmodules.xyz/objectstore-api@v0.24.0 \
+    #         -require=kmodules.xyz/offshoot-api@v0.24.2 \
+    #         -require=gomodules.xyz/x@v0.0.14 \
+    #         -require=gomodules.xyz/logs@v0.0.6 \
+    #         -require=k8s.io/kube-openapi@v0.0.0-20220328201542-3ee0da9b0b42 \
+    #         -require=kmodules.xyz/resource-metadata@v0.12.5 \
+    #         -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
+    #         -require=gomodules.xyz/password-generator@v0.2.8 \
+    #         -require=go.bytebuilders.dev/license-verifier@v0.11.0 \
+    #         -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.11.0 \
+    #         -require=go.bytebuilders.dev/audit@v0.0.23 \
+    #         -require=stash.appscode.dev/apimachinery@v0.22.0 \
+    #         -require=go.mongodb.org/mongo-driver@v1.9.1 \
+    #         -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@v0.12.2-0.20220603144237-6cd001896bf3 \
+    #         -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.5 \
+    #         -replace=k8s.io/apimachinery=github.com/kmodules/apimachinery@v0.24.2-rc.0.0.20220603191800-1c7484099dee \
+    #         -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.0.0-20220603223637-59dad1716c43 \
+    #         -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@v1.25.0-alpha.0.0.20220603172133-1c9d09d1b3b8
+    #     go mod tidy
+    #     go mod vendor
+    # fi
     [ -z "$2" ] || (
         echo "$2"
         $2 || true
@@ -83,7 +96,7 @@ refresh() {
         else
             git commit -a -s -m "$COMMIT_MSG"
         fi
-        git push -u origin $PR_BRANCH -f
+        git push -u origin HEAD -f
         hub pull-request \
             --labels automerge \
             --message "$COMMIT_MSG" \
